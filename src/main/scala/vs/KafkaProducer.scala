b65a6fac3ef432c49a35a33520ef79eac5b281f6
objektwerks <objektwerks@gmail.com>
2015-07-27 13:27:58 -0400

    produce word messages, not count

--- src/main/scala/vs/KafkaProducer.scala
+++ src/main/scala/vs/KafkaProducer.scala
@@ -2,10 +2,7 @@ package vs
 
 import java.util.Properties
 
-import kafka.admin.AdminUtils
 import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
-import kafka.utils.ZKStringSerializer
-import org.I0Itec.zkclient.ZkClient
 
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
@@ -17,26 +14,15 @@ class KafkaProducer {
   private val topic = AppInstance.topic
   private val context = AppInstance.context
 
-  def send(): Unit = {
-    createTopic
+  def send(): Int = {
     val producer = new Producer[String, String](config)
     val rdd = context.makeRDD(AppInstance.license)
-    val wordCounts = countWords(rdd).collect()
+    val words = toWords(rdd).collect()
     val messages = ArrayBuffer[KeyedMessage[String, String]]()
-    wordCounts.foreach { wc =>
-      messages += KeyedMessage[String, String](topic = topic, key = wc._1, partKey = wc._1, message = wc._2.toString)
+    words foreach { w =>
+      messages += KeyedMessage[String, String](topic = topic, key = w, partKey = w, message = w)
     }
     producer.send(messages:_*)
-    println(s"Sent ${messages.size} messages to Kafka topic: $topic.")
-  }
-
-  private def createTopic(): Unit = {
-    val zkClient = new ZkClient("localhost:2181", 3000, 3000, ZKStringSerializer)
-    val metadata = AdminUtils.fetchTopicMetadataFromZk(topic, zkClient)
-    metadata.partitionsMetadata.foreach(println)
-    if (metadata.topic != topic) {
-      AdminUtils.createTopic(zkClient, topic, 1, 1)
-      println(s"Created topic: $topic")
-    }
+    messages.size
   }
 }
