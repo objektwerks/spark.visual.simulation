69826f49f10456ef82226169a4262fe67da16f2b
objektwerks <objektwerks@gmail.com>
2015-07-27 14:48:45 -0400

    removed method, folded into other method

--- src/main/scala/vs/KafkaProducer.scala
+++ src/main/scala/vs/KafkaProducer.scala
@@ -18,16 +18,12 @@ class KafkaProducer {
   def send(): Int = {
     val producer = new Producer[String, String](config)
     val rdd = context.makeRDD(AppInstance.license)
-    val words = toWords(rdd).collect()
+    val words = rdd.flatMap(l => l.split("\\P{L}+")).filter(_.nonEmpty).map(_.toLowerCase).collect()
     val messages = ArrayBuffer[KeyedMessage[String, String]]()
     words foreach { w =>
       messages += KeyedMessage[String, String](topic = topic, key = w, partKey = w, message = 1.toString)
     }
     producer.send(messages:_*)
     messages.size
-  }
-
-  private def toWords(rdd: RDD[String]): RDD[(String)] = {
-    rdd.flatMap(l => l.split("\\P{L}+")).filter(_.nonEmpty).map(_.toLowerCase)
   }
 }
