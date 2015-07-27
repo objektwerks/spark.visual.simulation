848f29e1aea417686e5cfd2b7dd748acea391c9c
objektwerks <objektwerks@gmail.com>
2015-07-27 13:27:26 -0400

    added toWords function

--- src/main/scala/vs/package.scala
+++ src/main/scala/vs/package.scala
@@ -2,6 +2,10 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.DStream
 
 package object vs {
+  def toWords(rdd: RDD[String]): RDD[(String)] = {
+    rdd.flatMap(l => l.split("\\P{L}+")).filter(_.nonEmpty).map(_.toLowerCase)
+  }
+
   def countWords(rdd: RDD[String]): RDD[(String, Int)] = {
     rdd.flatMap(l => l.split("\\P{L}+")).filter(_.nonEmpty).map(_.toLowerCase).map(w => (w, 1)).reduceByKey(_ + _)
   }
