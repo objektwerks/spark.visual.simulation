a3debe90e19a5bbd23f41ae20725ae6fe069878a
objektwerks <objektwerks@gmail.com>
2015-07-29 14:31:17 -0400

    added println to simulation test

--- src/test/scala/vs/SimulationTest.scala
+++ src/test/scala/vs/SimulationTest.scala
@@ -6,6 +6,7 @@ class SimulationTest extends FunSuite {
   test("simulation") {
     val simulation = new Simulation()
     val result = simulation.play()
+    println(result.toString)
     assert(result.kafkaMessages.nonEmpty)
     assert(result.cassandraMessages.nonEmpty)
     assert(result.cassandraRatings.nonEmpty)
