5a984bcf04dd40388f209e6d5436630054b57abb
objektwerks <objektwerks@gmail.com>
2015-08-13 13:22:09 -0400

    removed unnecessary execution context from older future code

--- src/test/scala/vs/SimulationTest.scala
+++ src/test/scala/vs/SimulationTest.scala
@@ -2,11 +2,7 @@ package vs
 
 import org.scalatest.FunSuite
 
-import scala.concurrent.ExecutionContext
-
 class SimulationTest extends FunSuite {
-  implicit def ec = ExecutionContext.global
-
   test("simulation") {
     val simulation = new Simulation()
     val result = simulation.play()
