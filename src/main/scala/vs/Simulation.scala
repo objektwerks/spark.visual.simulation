bfea0d618db14a0d817c26097e1c03809ffd342a
objektwerks <objektwerks@runbox.com>
2016-12-06 14:13:56 -0500

    refactored spark conf using spark session; also use cassandra driver to setup keyspace and table

--- src/main/scala/vs/Simulation.scala
+++ src/main/scala/vs/Simulation.scala
@@ -3,8 +3,8 @@ package vs
 import java.util.Properties
 import java.util.concurrent.TimeUnit
 
+import com.datastax.driver.core.Cluster
 import com.datastax.spark.connector.SomeColumns
-import com.datastax.spark.connector.cql.CassandraConnector
 import kafka.admin.AdminUtils
 import kafka.utils.ZkUtils
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
@@ -12,7 +12,6 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.{KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Milliseconds, StreamingContext}
-import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
@@ -22,26 +21,31 @@ case class Result(ratings: Seq[(String, String, String, String)], // Source
                   programRatings: Seq[(String, Long)]) // Sink
 
 class Simulation {
-  val sparkConf = new SparkConf(loadDefaults = true)
-    .setMaster("local[2]")
-    .setAppName("simulation")
-    .set("spark.cassandra.connection.host", "127.0.0.1")
-    .set("spark.cassandra.auth.username", "cassandra")
-    .set("spark.cassandra.auth.password", "cassandra")
-  val sparkContext = new SparkContext(sparkConf)
-  val sparkSession = SparkSession.builder.config(sparkConf).getOrCreate()
+  val cluster = Cluster.builder.addContactPoint("127.0.0.1").build()
+  val session = cluster.connect()
+  session.execute("DROP KEYSPACE IF EXISTS simulation;")
+  session.execute("CREATE KEYSPACE simulation WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
+  session.execute("CREATE TABLE simulation.ratings(program text, season int, episode int, rating int, PRIMARY KEY (program, season, episode));")
+
+  val spark = SparkSession.builder
+    .master("local[2]")
+    .appName("visual.spark")
+    .config("spark.cassandra.connection.host", "127.0.0.1")
+    .config("spark.cassandra.auth.username", "cassandra")
+    .config("spark.cassandra.auth.password", "cassandra")
+    .getOrCreate()
+
   val kafkaProducerProperties = loadProperties("/kafka.producer.properties")
   val kafkaConsumerProperties = toMap(loadProperties("/kafka.consumer.properties"))
   val kafkaTopic = "ratings"
 
   def play(): Result = {
     createKafkaTopic()
-    createCassandraKeyspaceAndTable()
     val ratings = produceAndSendKafkaTopicMessages()
     consumeKafkaTopicMessagesAsDirectStream()
     val programToEpisodesRatings = selectProgramToEpisodesRatingsFromCassandra()
     val programRatings = selectProgramRatingsFromCassandra()
-    sparkContext.stop()
+    spark.stop()
     Result(ratings, programToEpisodesRatings, programRatings)
   }
 
@@ -53,15 +57,6 @@ class Simulation {
     if (topicMetadata.topic != kafkaTopic) {
       AdminUtils.createTopic(zkUtils, kafkaTopic, 1, 1, kafkaProducerProperties)
       println(s"Kafka Topic ( $kafkaTopic ) created.")
-    }
-  }
-
-  def createCassandraKeyspaceAndTable(): Unit = {
-    val connector = CassandraConnector(sparkConf)
-    connector.withSessionDo { session =>
-      session.execute("DROP KEYSPACE IF EXISTS simulation;")
-      session.execute("CREATE KEYSPACE simulation WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 };")
-      session.execute("CREATE TABLE simulation.ratings(program text, season int, episode int, rating int, PRIMARY KEY (program, season, episode));")
     }
   }
 
@@ -84,7 +79,7 @@ class Simulation {
   // Flow
   def consumeKafkaTopicMessagesAsDirectStream(): Unit = {
     import com.datastax.spark.connector.streaming._
-    val streamingContext = new StreamingContext(sparkContext, Milliseconds(10000))
+    val streamingContext = new StreamingContext(spark.sparkContext, Milliseconds(10000))
     val kafkaParams = kafkaConsumerProperties
     val kafkaTopics = Set(kafkaTopic)
     val is = KafkaUtils.createDirectStream[String, String](
@@ -105,7 +100,7 @@ class Simulation {
 
   // Flow
   def selectProgramToEpisodesRatingsFromCassandra(): Map[String, Seq[(Int, Int)]] = {
-    val dataframe = sparkSession.sql("select program, episode, rating from simulation.ratings")
+    val dataframe = spark.sql("select program, episode, rating from simulation.ratings")
     val rows = dataframe.orderBy("program", "episode", "rating").collect()
     val data = new ArrayBuffer[(String, Int, Int)](rows.length)
     rows foreach { row =>
@@ -117,7 +112,7 @@ class Simulation {
 
   // Sink
   def selectProgramRatingsFromCassandra(): Seq[(String, Long)] = {
-    val dataframe = sparkSession.sql("select program, rating from simulation.ratings")
+    val dataframe = spark.sql("select program, rating from simulation.ratings")
     val rows = dataframe.groupBy("program").agg("rating" -> "sum").orderBy("program").collect()
     val data = new ArrayBuffer[(String, Long)](rows.length)
     rows foreach { row =>
