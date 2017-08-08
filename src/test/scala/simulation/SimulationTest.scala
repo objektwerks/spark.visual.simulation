2bcc5bfd92f87937611a30dad9994e08a9cb35bb
objektwerks <objektwerks@runbox.com>
2017-08-08 10:02:23 -0400

    fixed reorg issues

---
+++ src/test/scala/simulation/SimulationTest.scala
@@ -0,0 +1,14 @@
+package simulation
+
+import org.scalatest.FunSuite
+
+class SimulationTest extends FunSuite {
+  test("simulation") {
+    val simulation = new Simulation()
+    val result = simulation.play()
+    assert(result.ratings.nonEmpty)
+    assert(result.programToEpisodesRatings.nonEmpty)
+    assert(result.programRatings.nonEmpty)
+    println(result.toString)
+  }
+}
